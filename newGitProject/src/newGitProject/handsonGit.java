0bd74ce25ff2d2a793d212584efe1a8656b6885c
922645 <922645@LTSA000373.cts.com>
2021-08-02 17:18:00 +0300

    changes in second branch

--- newGitProject/src/newGitProject/handsonGit.java
+++ newGitProject/src/newGitProject/handsonGit.java
@@ -2,4 +2,13 @@ package newGitProject;
 
 public class handsonGit {
 	int i ;
+	public void method2(){
+		System.out.println("method2");
+	}
+	public void method3(){
+		System.out.println("method3");
+	}
+	public void method4(){
+		System.out.println("method4");
+	}
 }
